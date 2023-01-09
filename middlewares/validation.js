06f4d6fb002bc16a29b1369aa47d8673838483f7
Valeriy Nizovtsev <nizucer123@gmail.com>
2023-01-09 21:39:01 +0200

    hw3 fix

---
+++ middlewares/validation.js
@@ -0,0 +1,12 @@
+const { ServiceError } = require("../helpers");
+
+const validation = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    next(new ServiceError(400, error.message));
+    return;
+  }
+  next();
+};
+
+module.exports = validation;
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
+mongoose
+  .connect('mongodb+srv://niza33:123456VVv@cluster0.k2zhwys.mongodb.net/test')
+  .then(console.log("Database connection successful"))
+  .then(() => {
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+})
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
