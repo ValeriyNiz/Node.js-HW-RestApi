d6625ee65392a5ba8e1354d6c52b27f7d98c6c2f
ValeriyNiz <nizucer123@gmail.com>
2023-01-10 12:57:18 +0200

    Update validationId.js

--- middlewares/validationId.js
+++ middlewares/validationId.js
@@ -6,7 +6,7 @@ const validationId = (req, res, next) => {
   const { id } = req.params;
   const isValid = isValidObjectId(id);
   if (!isValid) {
-    next(ServiceError(404, "Invalid id"));
+    next(new ServiceError(404, "Invalid id"));
     return;
   }
   next();
