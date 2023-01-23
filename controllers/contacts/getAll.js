82509c7ecd9eb51e5dba66fecdaa1b3040f0f5b4
Valeriy Nizovtsev <nizucer123@gmail.com>
2023-01-23 18:09:19 +0200

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,7 @@ const { Contact } = require("../../models/contacts");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite = true } = req.query;
+  const { page = 1, limit = 20, favorite = (true, false) } = req.query;
   const skip = (page - 1) * limit;
 
   const contacts = await Contact.find({owner: _id, favorite: favorite}, "-createdAt -updatedAt", {skip,
